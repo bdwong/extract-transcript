f0cc93fa2afef72c1c0311d9005c42f8ce49f527
Brian D. Wong <bdwong.net@gmail.com>
2023-05-02 16:38:07 -0700

    Add .gitignore, keywords, modules: appyconfig, test

---
+++ tests/tests.js
@@ -0,0 +1,11 @@
+const { mock, test, describe, beforeEach, it } = require('test');
+const assert = require('node:assert/strict');
+
+// TODO: load test config.
+// TODO: Require library functions to test
+
+describe('test setup', () => {
+  it('passes a test', () => {
+    assert.equal(true, true);
+  })
+})
