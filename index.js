e6bf0df3e6532a879fc69492de5ae2553980beff
Brian D. Wong <bdwong.net@gmail.com>
2023-05-03 11:04:55 -0700

    Add commander module. Process files from command line.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const config = require('lib/config');
+const config = require('./lib/config');
+const { program } = require('commander');
+const { extractWithTimestamps } = require('./lib/extractor');
+const fs = require('node:fs');
+const path = require('node:path');
+
+program.option('-i, --interval <seconds>', 'print timestamp after specific interval (in seconds)')
+program.parse();
+
+const options = program.opts();
+
+program.args.forEach(filepath => {
+  const transcript = JSON.parse(fs.readFileSync(filepath));
+  console.log(extractWithTimestamps(transcript));
+});
