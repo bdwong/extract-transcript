f1318d0ea1fcf6e47d5c3d2602349f69370cdb16
Brian D. Wong <bdwong.net@gmail.com>
2023-05-06 06:06:06 -0700

    Add command line option to output srt and text formats.

--- extract-transcript.js
+++ extract-transcript.js
@@ -1,20 +1,43 @@
 const config = require('./lib/config');
-const { program } = require('commander');
-const { extractWithTimestamps, extractWithBlockInterval } = require('./lib/extractor');
+const { program, Option } = require('commander');
+const {
+  formatAsText,
+  formatAsTranscript,
+  formatAsSRT,
+  quantizeBlocks,
+ } = require('./lib/extractor');
 const fs = require('node:fs');
 const path = require('node:path');
 
-program.option('-i, --interval <seconds>', 'print timestamp after specific interval (in seconds)')
+program.option('-i, --interval <seconds>', 'print timestamp after specific interval (in seconds)');
+program.addOption(new Option('-f, --format <type>', 'output format')
+  .choices(['txt', 'transcript', 'srt'])
+  .default('transcript')
+);
 program.parse();
 
 const options = program.opts();
 
+let formatter;
+switch(options.format) {
+  case 'txt':
+    formatter = formatAsText;
+    break;
+  case 'transcript':
+    formatter = formatAsTranscript;
+    break;
+  case 'srt':
+    formatter = formatAsSRT;
+    break;
+}
+
 program.args.forEach(filepath => {
   const transcript = JSON.parse(fs.readFileSync(filepath));
 
+  let blocks = transcript[0];
+
   if (options.interval) {
-    console.log(extractWithBlockInterval(transcript, options.interval));
-  } else {
-    console.log(extractWithTimestamps(transcript));
+    blocks = quantizeBlocks(blocks, options.interval*1000);
   }
+  console.log(formatter(blocks));
 });
